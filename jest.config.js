682aca56421d869284cf93c0b8fa793a14ef2309
Graham McCulloch <admin@grahammcculloch.co.uk>
2021-07-14 14:29:48 +0700

    Use config from jellyfish-config
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@
  * Proprietary and confidential.
  */
 
+const base = require('@balena/jellyfish-config/config/jest.config')
+
 module.exports = {
-	verbose: true,
-	preset: "ts-jest",
-	testEnvironment: "node",
+	...base,
 	testTimeout: 20000, // DB connection/setup seems to be very slow sometimes in CI
 	maxWorkers: 1,
 	forceExit: true
