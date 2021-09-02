eeaec8e71801ea3f4fbe2d4536d5e86f847ad0be
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-09-02 16:01:13 +0100

    Drop the slugify module dependency in favor of simple inline code
    Using the @sindresorhus/slugify module was causing a lot of headaches as
    it is shipped as an esm module and requires additional compiler and jest
    config. The additional effort wasn't worth the convenience, so I decided
    to replace it with a simple inline statement that is functionally the
    same.
    
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -10,13 +10,5 @@ module.exports = {
 	...base,
 	testTimeout: 20000, // DB connection/setup seems to be very slow sometimes in CI
 	maxWorkers: 1,
-	forceExit: true,
-	transformIgnorePatterns: [
-			// all exceptions must be first line
-		"/node_modules/(?!@sindresorhus/(.*)|escape-string-regexp)",
-	],
-	transform: {
-		"/node_modules/@sindresorhus/(.*)": 'jest-esm-transformer',
-		"/node_modules/escape-string-regexp/(.*)": 'jest-esm-transformer'
-	}
+	forceExit: true
 };
