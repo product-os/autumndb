bb793d5f85912fe4ac40b65092f16987b09b2a04
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-09-06 16:29:24 +0000

    Add testing for full-text search functionality
    These tests are mostly an adaptation of the jellyfish repo tests, which
    test at the wrong abstraction level. By testing at the kernel level we
    ensure the gaurantess of the kernel interface and avoid unintended
    breakages.
    See https://github.com/product-os/jellyfish/blob/v38.2.3/test/e2e/server/full-text-search.spec.js
    
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- test/integration/kernel.spec.ts
+++ test/integration/kernel.spec.ts
@@ -6298,6 +6298,386 @@ describe('Kernel', () => {
 				ctx.kernel.getCardBySlug(ctx.context, session.id, 'user-admin@1.0.0'),
 			).rejects.toThrow();
 		});
+
+		it('should be able to query root level string fields using full text search', async () => {
+			const name = 'lorem ipsum dolor sit amet';
+			const type = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'type@1.0.0',
+					data: {
+						schema: {
+							type: 'object',
+							properties: {
+								name: {
+									type: 'string',
+									fullTextSearch: true,
+								},
+							},
+						},
+					},
+				},
+			);
+
+			const contract = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: `${type.slug}@${type.version}`,
+					name,
+				},
+			);
+			await ctx.kernel.insertCard(ctx.context, ctx.kernel.sessions!.admin, {
+				type: `${type.slug}@${type.version}`,
+				name: 'foobar',
+			});
+
+			const results = await ctx.kernel.query(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'object',
+					properties: {
+						type: {
+							type: 'string',
+							const: `${type.slug}@${type.version}`,
+						},
+						name: {
+							type: 'string',
+							fullTextSearch: {
+								// Reverse the term so its not an exact match
+								term: name.split(' ').reverse().join(' '),
+							},
+						},
+					},
+				} as any,
+			);
+
+			expect(results).toHaveLength(1);
+			expect(results[0].id).toBe(contract.id);
+		});
+
+		it('should be able to query root level array fields using full text search', async () => {
+			const tag = 'lorem';
+			const type = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'type@1.0.0',
+					data: {
+						schema: {
+							type: 'object',
+							properties: {
+								tags: {
+									type: 'array',
+									fullTextSearch: true,
+									items: {
+										type: 'string',
+									},
+								},
+							},
+						},
+					},
+				},
+			);
+
+			const contract = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: `${type.slug}@${type.version}`,
+					tags: [tag],
+				},
+			);
+			await ctx.kernel.insertCard(ctx.context, ctx.kernel.sessions!.admin, {
+				type: `${type.slug}@${type.version}`,
+			});
+
+			const results = await ctx.kernel.query(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'object',
+					properties: {
+						tags: {
+							type: 'array',
+							contains: {
+								fullTextSearch: {
+									term: tag,
+								},
+							},
+						},
+					},
+					required: ['tags'],
+				} as any,
+			);
+
+			expect(results).toHaveLength(1);
+			expect(results[0].id).toBe(contract.id);
+		});
+
+		it('should be able to query nested string fields using full text search', async () => {
+			const description = 'lorem ipsum dolor sit amet';
+			const type = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'type@1.0.0',
+					data: {
+						schema: {
+							type: 'object',
+							properties: {
+								data: {
+									type: 'object',
+									properties: {
+										description: {
+											fullTextSearch: true,
+											type: 'string',
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			);
+
+			const contract = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: `${type.slug}@${type.version}`,
+					data: {
+						description,
+					},
+				},
+			);
+			await ctx.kernel.insertCard(ctx.context, ctx.kernel.sessions!.admin, {
+				type: `${type.slug}@${type.version}`,
+			});
+
+			const results = await ctx.kernel.query(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'object',
+					required: ['data'],
+					properties: {
+						type: {
+							type: 'string',
+							const: `${type.slug}@${type.version}`,
+						},
+						data: {
+							type: 'object',
+							required: ['description'],
+							properties: {
+								description: {
+									type: 'string',
+									fullTextSearch: {
+										// Reverse the term so its not an exact match
+										term: description.split(' ').reverse().join(' '),
+									},
+								},
+							},
+						},
+					},
+				} as any,
+			);
+
+			expect(results).toHaveLength(1);
+			expect(results[0].id).toBe(contract.id);
+		});
+
+		it('should be able to query nested array fields using full text search', async () => {
+			const label = 'lorem';
+			const type = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'type@1.0.0',
+					data: {
+						schema: {
+							type: 'object',
+							properties: {
+								data: {
+									type: 'object',
+									properties: {
+										labels: {
+											type: 'array',
+											fullTextSearch: true,
+											items: {
+												type: 'string',
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			);
+
+			const contract = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: `${type.slug}@${type.version}`,
+					data: {
+						labels: [label],
+					},
+				},
+			);
+			await ctx.kernel.insertCard(ctx.context, ctx.kernel.sessions!.admin, {
+				type: `${type.slug}@${type.version}`,
+			});
+
+			const results = await ctx.kernel.query(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'object',
+					properties: {
+						type: {
+							type: 'string',
+							const: `${type.slug}@${type.version}`,
+						},
+						data: {
+							type: 'object',
+							required: ['labels'],
+							properties: {
+								labels: {
+									type: 'array',
+									contains: {
+										fullTextSearch: {
+											term: label,
+										},
+									},
+								},
+							},
+						},
+					},
+					required: ['data'],
+				} as any,
+			);
+
+			expect(results).toHaveLength(1);
+			expect(results[0].id).toBe(contract.id);
+		});
+
+		it('should be able to query deeply nested string fields using full text search', async () => {
+			const description = 'lorem ipsum dolor sit amet';
+			const type = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'type@1.0.0',
+					data: {
+						schema: {
+							type: 'object',
+							properties: {
+								data: {
+									type: 'object',
+									properties: {
+										nested: {
+											type: 'object',
+											properties: {
+												nested: {
+													type: 'object',
+													properties: {
+														nested: {
+															type: 'object',
+															properties: {
+																description: {
+																	fullTextSearch: true,
+																	type: 'string',
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			);
+
+			const contract = await ctx.kernel.insertCard(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: `${type.slug}@${type.version}`,
+					data: {
+						nested: {
+							nested: {
+								nested: {
+									description,
+								},
+							},
+						},
+					},
+				},
+			);
+			await ctx.kernel.insertCard(ctx.context, ctx.kernel.sessions!.admin, {
+				type: `${type.slug}@${type.version}`,
+			});
+
+			const results = await ctx.kernel.query(
+				ctx.context,
+				ctx.kernel.sessions!.admin,
+				{
+					type: 'object',
+					required: ['data'],
+					properties: {
+						type: {
+							type: 'string',
+							const: `${type.slug}@${type.version}`,
+						},
+						data: {
+							type: 'object',
+							required: ['nested'],
+							properties: {
+								nested: {
+									type: 'object',
+									required: ['nested'],
+									properties: {
+										nested: {
+											type: 'object',
+											required: ['nested'],
+											properties: {
+												nested: {
+													type: 'object',
+													required: ['description'],
+													properties: {
+														description: {
+															type: 'string',
+															fullTextSearch: {
+																// Reverse the term so its not an exact match
+																term: description
+																	.split(' ')
+																	.reverse()
+																	.join(' '),
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				} as any,
+			);
+
+			expect(results).toHaveLength(1);
+			expect(results[0].id).toBe(contract.id);
+		});
 	});
 
 	describe('.stream()', () => {
