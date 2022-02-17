23c5848508ca233109fcdce0a061b0306b42fe8b
edufschmidt <eduardo@balena.io>
2022-02-17 18:02:05 +0000

    Replace SelectObject interface definition

--- lib/backend/postgres/types.ts
+++ lib/backend/postgres/types.ts
@@ -7,15 +7,8 @@ export interface SearchFieldDef {
 	path: string[];
 	isArray: boolean;
 }
-
-// TS-TODO: strongly type this object
 export interface SelectObject {
-	type?: { [key: string]: any };
-	data?: { [key: string]: any };
-	id?: { [key: string]: any };
-	slug?: { [key: string]: any };
-	links?: { [key: string]: any };
-	properties?: { [key: string]: any };
+	[key: string]: SelectObject;
 }
 
 export interface BackendQueryOptions extends Omit<QueryOptions, 'mask'> {
