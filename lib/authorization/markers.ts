8bdf59543abee36ca0f5abfd26ea1345a434ba2b
Josh Bowling <josh@balena.io>
2022-02-22 15:17:50 +0900

    Fix markers org query
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/authorization/markers.ts
+++ lib/authorization/markers.ts
@@ -30,8 +30,8 @@ export const resolveMarkerBasedAuthorizationSchema = async (
 							type: 'string',
 							const: actor.id,
 						},
-						additionalProperties: false,
 					},
+					additionalProperties: false,
 				},
 			},
 			properties: {
