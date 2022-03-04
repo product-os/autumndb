8c4e342019b82b2cde291ba053452dc1d1708e67
Josh Bowling <josh@balena.io>
2022-03-04 15:05:24 +0900

    Use skhema.SchemaMismatch error
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/json-schema.ts
+++ lib/json-schema.ts
@@ -1,8 +1,4 @@
 import * as skhema from 'skhema';
-import * as errors from './errors';
-
-// TS-TODO: Is this 100% necessary??
-(skhema as any).SchemaMismatch = errors.JellyfishSchemaMismatch;
 
 // TS-TODO: the skhema lib expects v6 schemas, rather than the JsonSchemaQL schemas
 // used in Jellyfish
