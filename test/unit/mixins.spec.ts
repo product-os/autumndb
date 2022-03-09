a30e09e3a502d37251e9f146baa326ce00212f6c
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-03-09 10:49:35 +0000

    mixins: Add unit test for .withEvents()
    This change adds a simple unit test to sanity check the formula used for
    aggregating values in the `withEvents()` mixin. This change highlights
    that the core should probably not be providing mixins, especialy with
    functionality that it doesn't provide or handle directly ($$formula
    fields). In the future we should consider hoisting mixins into the
    worker module to improve feature seperation and make the system less
    brittle.
    
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

---
+++ test/unit/mixins.spec.ts
@@ -0,0 +1,37 @@
+import { cardMixins } from '../../lib';
+import type { JsonSchema } from '@balena/jellyfish-types';
+import { evaluateObject } from '@balena/jellyfish-jellyscript';
+
+describe('cardMixins', () => {
+	describe('.withEvents()', () => {
+		it('should create a valid formula', () => {
+			const typeContract = cardMixins.withEvents('test-type', 'type');
+			const schema: JsonSchema = typeContract.data.schema as any;
+			const sample: any = {
+				name: 'sample contract',
+				type: 'test-type',
+				slug: 'test-sample',
+				links: {
+					'has attached element': [
+						{
+							type: 'create',
+							slug: 'foo-1',
+						},
+						{
+							type: 'update@1.0.0',
+							slug: 'foo-2',
+						},
+						{
+							type: 'message@1.0.0',
+							slug: 'foo-3',
+							tags: ['test-tag'],
+						},
+					],
+				},
+			};
+			const result = evaluateObject(schema, sample);
+
+			expect(result.tags).toEqual(['test-tag']);
+		});
+	});
+});
