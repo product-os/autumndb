f2afa4032de2f4004d34e3b1f4bfd178fa03fa04
Josh Bowling <josh@balena.io>
2022-03-18 09:36:54 +0900

    Drop cardMixins export
    Dropping this alias in favor of contractMixins
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/index.ts
+++ lib/index.ts
@@ -2,8 +2,6 @@ export { PostgresBackendOptions, StreamChange } from './backend';
 export { Cache } from './cache';
 export { CONTRACTS, CARDS } from './contracts';
 export * as contractMixins from './contracts/mixins';
-// Remove this alias as soon as uses in depending packages have been removed.
-export * as cardMixins from './contracts/mixins';
 export * as errors from './errors';
 export { Kernel, QueryOptions } from './kernel';
 export * as testUtils from './test-utils';
