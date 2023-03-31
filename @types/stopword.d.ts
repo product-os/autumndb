b988ad89c7b9acfbea31808c797436d988a7838a
Josh Bowling <josh@monarci.com>
2023-04-01 07:44:48 +0900

    Update stopword, fix typings
    Change-type: patch

--- @types/stopword.d.ts
+++
@@ -1,9 +0,0 @@
-// TS-TODO: Contribute these type to the json-schema-deref-sync package
-declare module 'stopword' {
-	export function removeStopwords(
-		tokens: string[],
-		stopwords: string[],
-	): string[];
-
-	export const en: string[];
-}
